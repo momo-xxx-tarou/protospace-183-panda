a4246f85e753c5a43cf6f28ad734373b002f4688
momo-xxx-tarou <momo.xxx.xyz@gmail.com>
2024-11-07 18:39:35 +0900

    モデルとテーブルの再作成

--- db/migrate/20241107093616_create_prototypes.rb
+++ db/migrate/20241107093616_create_prototypes.rb
@@ -1,6 +1,6 @@
-class CreateProducts < ActiveRecord::Migration[7.0]
+class CreatePrototypes < ActiveRecord::Migration[7.0]
   def change
-    create_table :products do |t|
+    create_table :prototypes do |t|
       t.string :name
       t.text :copy
       t.text :concept

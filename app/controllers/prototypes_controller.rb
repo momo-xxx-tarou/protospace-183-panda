47b39844bc1f079f42db672b58204f39fceb11f6
Yo-Kaminagi0913 <yamazaki1029@yahoo.co.jp>
2024-11-11 16:33:15 +0900

    Active Storageの導入

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,4 +3,9 @@ class PrototypesController < ApplicationController
   def index
   end
 
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
 end

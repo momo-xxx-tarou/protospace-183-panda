3b80dc2e28c0d5392e1af2494a66a91798331693
Yo-Kaminagi0913 <yamazaki1029@yahoo.co.jp>
2024-11-11 17:36:06 +0900

    プロトタイプ投稿ページの実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -4,11 +4,16 @@ class PrototypesController < ApplicationController
   end
 
   def new
-    
+    @prototype = Prototype.new
   end
 
   def create
-    
+    @prototype = Prototype.create(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private

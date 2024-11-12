75f6f7ceb66506a38886d5996e28e3d718f23cdf
Yo-Kaminagi0913 <yamazaki1029@yahoo.co.jp>
2024-11-12 21:03:21 +0900

    非ログイン時のログイン画面へのリダイレクトと非投稿者のトップページへのリダイレクトの実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :move_to_index, only: [:edit]
   def index
     @prototypes = Prototype.all
   end
@@ -46,5 +47,12 @@ class PrototypesController < ApplicationController
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @prototype = Prototype.find(params[:id])
+    unless current_user.id == @prototype.user.id
+      redirect_to action: :index
+    end
   end
 end
